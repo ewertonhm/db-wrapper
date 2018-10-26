6d7e960da95e79e9c04110ade6454b48e207bf03
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2018-10-26 04:43:53 -0300

    Documentação

--- README.md
+++ README.md
@@ -1,3 +1,73 @@
-# db wrapper
+## db wrapper
 
-Documentação disponivel no arquivo dbControl.php
+> **a database management class writen in php**
+
+ - Generic Query Run
+ - Insert
+ - Update
+ - Find
+ - Delete
+
+> **Documentation**
+*only in pt-br at the moment, if someone agrees to translate, feel free*	
+- **Configurando sua conexão com o banco de dados**
+As configurações do DB vão no metodo construtor da classe, localizado no arquivo DB.php
+linha 20: `$this->_pdo = new PDO('pgsql:host=IPDOSERVIDOR;port=PORTA;dbname=NOMEDOBANCO','USUARIO','SENHA');`
+
+ - **Chamando / Instanciando o Objeto** *função get_instance()*
+  `$db = DB::get_instance();`
+A classe conta com uma função que verifica se o objeto já foi instanciado, caso não, instancia e retorna o objeto, caso sim, retorna o objeto que já foi instanciado anteriormente, assim evitando de se criar diversas instancias com o banco de dados.
+
+ - **Executando querys SQL** *função query()*
+`$query = $db->query("SELECT * FROM contacts ORDER BY lname, fname");`
+`$contacts = $query->get_results();`
+A função *query()* recebe e roda um SQL, pode ser usada para inserção, nesse caso a função conta com um sistema de bind de valores para seguraça contra SQL Injection. 
+Para inserir:
+Crie um array com os valores a ser inseridos no banco
+`$contact = ['Ewerton','Marschalk','Ewerton@hotmail.com','44444444444','5555555555555'];`
+Na hora de chamar a função, passe o SQL Statement e o Array, no SQL ao invés de adicionar os valores, adicione pontos de interrogação, os mesmos serão preenchidos com os dados do array.
+`$db->query("INSERT INTO contacts (fname, lname, email, cell_phone, home_phone) VALUES (?, ?, ?, ?, ?)",$contact);`
+
+- **Inserindo dados no banco** *Função insert()*
+A função query recebe o nome da tabela e um array com os campos e os dados a serem inseridos.
+Exemplo do array:
+`$contact = ['fname'=>'fharu','lname'=>'lharu','email'=>'haru@haru.com','cell_phone'=>'123 456 7890','home_phone'=>'098 765 4321'];`
+Chamando a função:
+`$db->insert('contacts',$contact);`
+Retorna true ou false.
+- **Atualizando dados do banco** *Função update()*
+A função update recebe o nome da tabela, id e array de campos a serem modificados.
+`$contact = ['fname'=>'HARU','email'=>'haru@haru.com'];`
+`$db->update('contacts',1,$contact);`
+Retorna true ou false
+
+- **Deletando dados do banco** *Função delete()*
+A função dele recebe a tabela e o id a ser deletado
+`$db->delete(contacts,1);`
+Retorna True ou False
+
+- **Consultas** *Funções find() e findFirst()*
+A função find recebe o nome da tabela e opcionalmente um array com parametros para a procura.
+Dos campos do array: 
+	• join: nome das tabelas para fazer inner joins
+	• bindJoin: PKs e FKs para o inner join, exemplo: `produto.cod_album = album.id`
+	• conditions: campos do WHERE, exemplo: `lname = ?`
+	• bind: valores pra buscar no where, vai dar bind nos pontos de interrogação do conditions
+	• order: valores para orderby
+	• limit: limita o numero de resultados
+	exemplo de array:
+	`$params = [`
+	`'joins' => ['album','midia'],`
+`'bindjoin' => ['produto.cod_album = album.id','produto.cod_midia = midia.id'],`
+   ` 'conditions' => ['lname = ?','fname = ?'],`
+`'bind' => ['lharu','fharu'],`
+`'order' => "fname Desc",`
+` 'limit' => 5`
+`];`
+chamando a função com o array:
+`$contacts = $db->find('contacts',$params);`
+`$resultados = $db->get_results;`
+A função retorna um Array de objetos com os resultados da consulta, caso não encontre nenhum resultado retorna false
+O array é completamente opicional, caso não seja passado um array o comando vai rodar um` SELECT * FROM TABELA`
+`$contacts = $db->find('contacts');`
+A função **findFirst()** funciona igual a Find, porém retorna apenas o primeiro objeto na forma de objeto
--- dbControl.php
+++ dbControl.php
@@ -5,100 +5,11 @@
  * @author Ewerton
  */
 interface dbControl {
-    // Verifica se o objeto PDO ja está instanciado, caso esteja, retorna o Objeto PDO, caso não, instancia e então retonar
     public static function get_instance();
-        // ex:
-            // $db recebe o objeto PDO
-            // $db = DB::get_instance();
-            
-    // função query
-    // Recebe e roda um SQL
-    // Pode receber um Array com valores para serem inseridos no Banco em caso do SQL ser um insert
-    // Ao passar um SQL de inserção, no lugar dos valores inserir ?, a função ira fazer um bind deles aos valores passados no Array 
     public function query();
-        // ex:
-            // array com os valores a serem inseridas no banco
-            // $contact = ['Ewerton','Marschalk','Ewerton@hotmail.com','44444444444','5555555555555'];
-
-            // chamando a função passando um SQL de inserção e um Array
-            // $db->query("INSERT INTO contacts (fname, lname, email, cell_phone, home_phone) VALUES (?, ?, ?, ?, ?)",$contact);
-
-            // chamando a função com um SQL de Select, o $query vai receber um objeto com o resultado
-            // $query = $db->query("SELECT * FROM contacts ORDER BY lname, fname");
-
-            // a função get_results é similar ao pg_fetch, retorna um array com os resultados
-            // $contacts = $query->get_results();
-    
-    // função Insert
-    // recebe o nome da tabela, e um array com o nome do campo e valor a ser inserido
-    // monta o Insert do SQL e roda a função query passando o SQL e os valores
-    // retorna true ou false;
     public function insert();
-        // ex:
-            // array com o nome do campo e valor
-            // $contact = ['fname'=>'fharu','lname'=>'lharu','email'=>'haru@haru.com','cell_phone'=>'123 456 7890','home_phone'=>'098 765 4321'];
-    
-            // função recebe o nome da tabela e o array
-            // $db->insert('contacts',$contact);
-    // função Update
-    // similar a função anterior, porém ao invés de inserir no banco, altera o valor 
-    // recebe o nome da tabela, id e campos a serem modificado com seus respectivos valores
-    // retorna true ou false;
     public function update();
-        // ex:
-            // array com os nomes dos campos e valores
-            // $contact = ['fname'=>'HARU','email'=>'haru@haru.com'];
-    
-            // função recebe tabela, id, e array 
-            // $db->update('contacts',1,$contact);
-    
-    // função Delete
-    // deleta um entrada do banco de dados, recebe a tabela e o Id
-    // retorna true ou false;
     public function delete();
-        // ex: 
-            // $db->delete(contacts,1);
-    // recebe um array com parametros
-    // joins é a tabela onde dar inner join
-    // binjoins é as PK e FKs
-    // condition é os campos para o WHERE
-    // bind é os valores dos campos no WHERE
-        // exemplo WHERE 'conditions' = 'bind'
-    // order é o valor do ORDER BY
-    // limit é o limite de resultados para exibir
-    // qualquer um dos campos pode ser deixado em branco
-    // retorna um array de objetos com o resultado
-    public function find();
-        // ex:
-            // exemplo de array de parametros
-            //$params = [
-            //    'joins' => ['album','midia'],
-            //    'bindjoin' => ['produto.cod_album = album.id','produto.cod_midia = midia.id'],
-            //    'conditions' => ['lname = ?','fname = ?'],
-            //    'bind' => ['lharu','fharu'],
-            //    'order' => "fname Desc",
-            //    'limit' => 5
-            //];
-
-            // chamando a função na tabela contacts com os parametros do array 
-            //$contacts = $db->find('contacts',$params);
-
-            //$resultados = $db->get_results;
-
-
-            // não é necessario passar nenhum parametro
-            // nesse caso o comando vai rodar um "SELECT * FROM tabela"
-            //$contacts = $db->find('contacts');
-    
-    // Função findFirst
-    // similar a função anterior, porém retorna apenas o primeiro resultado        
+    public function find(); 
     public function findFirst();
-        // ex:
-            //$params = [
-            //    'conditions' => ['lname = ?','fname = ?'],
-            //    'bind' => ['lharu','fharu'],
-            //    'order' => "fname Desc",
-            //    'limit' => 5
-            //];
-            //$contacts = $db->findFirst('contacts',$params);
 }
